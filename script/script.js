5cebdfa1ef2197c4d64a5af92b4bccd3e379d5dc
Kan-A-Pesh <michelm.gaming@gmail.com>
2023-03-09 00:15:47 +0100

    added model and texts

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 
 const renderer = new THREE.WebGLRenderer( { alpha: true } );
 renderer.setSize( window.innerWidth * 0.9, window.innerHeight * 0.9 );
@@ -8,6 +9,14 @@ threeCanvas.appendChild( renderer.domElement );
 
 
 const scene = new THREE.Scene();
+
+const light = new THREE.PointLight();
+light.position.set(0.8, 1.4, 1.0);
+scene.add(light);
+
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.enableDamping = true;
@@ -16,12 +25,31 @@ controls.enableZoom = false;
 controls.enablePan = false;
 
 controls.autoRotate = true;
-controls.autoRotateSpeed = 5;
+controls.autoRotateSpeed = 1;
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const fbxLoader = new FBXLoader()
+fbxLoader.load(
+    'assets/plateau_tris.fbx',
+    (object) => {
+        object.traverse(function (child) {
+            if (child.isMesh) {
+                // (child as THREE.Mesh).material = material
+                if (child.material) {
+                    child.material.transparent = false
+                }
+            }
+        })
+        object.scale.set(.00025, .00025, .00025)
+        object.position.set(0, -1, 0)
+        scene.add(object)
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
 
 camera.position.z = 2;
 controls.update();
